06b8eb455863a0c44d62b73a4cfe272bbb24a967
Hemanth <hemanthkarri009@gmail.com>
2022-02-01 14:12:17 -0500

    Second commit

---
+++ car.js
@@ -0,0 +1,204 @@
+let cars=[{
+    "maker":"acura",
+    "models":[{
+        "model":"ILX",
+        "price":"$14,950",
+        "mileage":"133,352 miles",
+        "transmission":"Automatic",
+        "exteriorColor":"Black",
+        "InteriorColor":"Black",
+        "gasMileage":["24 MPG In City","35 MPG On Highhway", "28 MPG Combined"],
+        "engine":"150 hp 2L I4",
+        "driveTrain":"Front-Wheel Drive",
+        "fuelType":"Gasoline",
+        "vin":"19VDE1F57EE014822",
+        "majorOptions":["Bluetooth", "Leather Seats", "Backup Camera","Sunroof/Moonroof", "Heated Seats", "Alloy Wheels"],
+        "carImage":"https://static.cargurus.com/images/forsale/2021/12/03/08/59/2014_acura_ilx-pic-2919899329564936570-1024x768.jpeg"
+    },{
+        "model":"MDX",
+        "price":"$31,746",
+        "mileage":"76,693 miles",
+        "transmission":"9-Speed Automatic",
+        "exteriorColor":"Black",
+        "InteriorColor":"Black",
+        "gasMileage":["18 MPG In City","26 MPG On Highway","21 MPG Combined"],
+        "engine":"2900 hp 3.5L V6",
+        "driveTrain":"All-Wheel Drive",
+        "fuelType":"Gasoline",
+        "vin":"5FRYD4H30HB006857",
+        "majorOptions":["Heated Seats", "Blind Spot Monitoring","Leather Seats", "Backup Camera","Sunroof/Moonroof", "Heated Seats", "Alloy Wheels","Adaptive Cruise Control"],
+        "carImage":"https://static.cargurus.com/images/forsale/2021/12/30/23/04/2017_acura_mdx-pic-8801954940555256830-1024x768.jpeg"
+    },{
+        "model":"NSX",
+        "price":"$137,995",
+        "mileage":"24,259 miles",
+        "transmission":"Automatic",
+        "exteriorColor":"Black",
+        "InteriorColor":"Black",
+        "gasMileage":["21 MPG In City","22 MPG On Highway","21 MPG Combined"],
+        "engine":"3.5L V6 Hybrid",
+        "driveTrain":"All-Wheel Drive",
+        "fuelType":"Hybrid  ",
+        "vin":"19UNC1B00HY000157",
+        "stock#":2736,
+        "majorOptions":["Navigation System  ", "Leather Seats", "Backup Camera"," Carbon Ceramic Brakes", "Adaptive Suspension", "Alloy Wheels", "Remote Start","Parking Sensors"],
+        "carImage":"https://static.cargurus.com/images/forsale/2021/12/28/01/32/2017_acura_nsx-pic-4295902107004887824-1024x768.jpeg"
+    }]
+},{
+    "maker":"Alfa Romeo",
+    "models":[{
+        "model":"Giulia",
+        "price":"$30,928",
+        "mileage":"24,219 miles",
+        "transmission":"8-Speed Automatic",
+        "exteriorColor":"Stromboli Gray Metallic",
+        "InteriorColor":"Black",
+        "gasMileage":["23 MPG In City","31 MPG On Highhway", "26 MPG Combined"],
+        "engine":"280 hp 2L I4",
+        "driveTrain":"All-Wheel Drive",
+        "fuelType":"Gasoline",
+        "vin":"ZARFANBN9K7603929",
+        "majorOptions":["Bluetooth", "Leather Seats", "Backup Camera","Sunroof/Moonroof", "Heated Seats", "Alloy Wheels","Parking Sensors"],
+        "carImage":"https://static.cargurus.com/images/forsale/2021/11/01/22/46/2019_alfa_romeo_giulia-pic-2707604396006544096-1024x768.jpeg"
+    },{
+        "model":"Stelvio",
+        "price":"$30,300",
+        "mileage":"87,999 miles",
+        "transmission":"8-Speed Automatic",
+        "exteriorColor":"Vulcano Black Metallic",
+        "InteriorColor":"Black",
+        "gasMileage":["22 MPG In City","28 MPG On Highhway", "24 MPG Combined"],
+        "engine":"280 hp 2L I4",
+        "driveTrain":"All-Wheel Drive",
+        "fuelType":"Gasoline",
+        "vin":"ZASFAKNN6J7B87252",
+        "majorOptions":["Bluetooth", "Leather Seats", "Backup Camera","Heated Seats", "Alloy Wheels"],
+        "carImage":"https://static.cargurus.com/images/forsale/2021/12/27/20/57/2018_alfa_romeo_stelvio-pic-7969055605028535788-1024x768.jpeg"
+    }]
+},{
+    "maker":"Audi",
+    "models":[{
+        "model":"A3",
+        "price":"$19,249",
+        "mileage":"58,035 miles",
+        "transmission":"Automatic",
+        "exteriorColor":"Blue",
+        "InteriorColor":"Black",
+        "engine":"170 hp 1.8L I4",
+        "driveTrain":"Front-Wheel Drive",
+        "fuelType":"Gasoline",
+        "vin":"WAUCCGFF9F1016951",
+        "majorOptions":["Bluetooth", "Leather Seats", "Backup Camera","Sunroof/Moonroof", "Heated Seats", "Alloy Wheels","LE Package","Cold Weather Package"],
+        "carImage":"https://static.cargurus.com/images/forsale/2021/12/07/20/23/2015_audi_a3-pic-8352404239722074392-1024x768.jpeg"
+    },{
+        "model":"A3 SportBack",
+        "price":"$23,798",
+        "mileage":"79,968 miles",
+        "transmission":"6-Speed Automatic",
+        "exteriorColor":"Mythos Black Metallic",
+        "InteriorColor":"Black",
+        "gasMileage":["34 MPG In City","39 MPG On Highhway", "36 MPG Combined"],
+        "engine":"204 hp 1.4L I4",
+        "driveTrain":"Front-Wheel Drive",
+        "fuelType":"Gasoline",
+        "vin":"WAUUPBFF8JA080077",
+        "majorOptions":["Bluetooth", "Leather Seats", "Backup Camera","Sunroof/Moonroof", "Heated Seats", "Alloy Wheels","Sport Package"],
+        "carImage":"https://static.cargurus.com/images/forsale/2021/08/06/12/52/2018_audi_a3_sportback-pic-5039166403400542736-1024x768.jpeg"
+
+    },{
+        "model":"A4",
+        "price":"$26,995",
+        "mileage":"44,615 miles",
+        "transmission":"7-Speed Automatic",
+        "exteriorColor":"Matador Red Metallic",
+        "InteriorColor":"Nougat Brown",
+        "engine":"252 hp 2L I4",
+        "driveTrain":"All-Wheel Drive",
+        "fuelType":"Gasoline",
+        "vin":"WAUENAF42JN012806",
+        "majorOptions":["Bluetooth", "Leather Seats", "Backup Camera","Sunroof/Moonroof", "Heated Seats", "Alloy Wheels","Premium wheels"],
+        "carImage":"https://static.cargurus.com/images/forsale/2021/08/06/12/52/2018_audi_a3_sportback-pic-5039166403400542736-1024x768.jpeg"
+    }]
+},{
+    "maker":"BMW",
+    "models":[{
+        "model":"1 Series",
+        "price":"$13,490",
+        "mileage":"86,372 miles",
+        "transmission":"Automatic",
+        "exteriorColor":"Silver",
+        "gasMileage":["18 MPG In City","27 MPG On Highhway", "22 MPG Combined"],
+       "engine":"230 hp 3L I6",
+        "driveTrain":"Rear-Wheel Drive",
+        "fuelType":"Gasoline",
+        "vin":"WBAUL7C50DVM83732",
+        "majorOptions":["Bluetooth","Alloy Wheels"],
+        "carImage":"https://static.cargurus.com/images/forsale/2021/12/12/04/20/2013_bmw_1_series-pic-8061172073952686785-1024x768.jpeg"
+    }]
+
+},{
+    "maker":"Buick",
+    "models":[{
+        "model":"Cascada",
+        "price":"$25,990",
+        "mileage":"14,223 miles",
+        "transmission":"6-Speed Automatic Overdrive",
+        "exteriorColor":"White",
+        "interiorColor":"Beige",
+        "gasMileage":["20 MPG In City","27 MPG On Highhway", "23 MPG Combined"],
+       "engine":"230 hp 3L I6",
+        "driveTrain":"Front-Wheel Drive",
+        "fuelType":"Gasoline",
+        "vin":" W04WT3N58GG143510",
+        "majorOptions":["Bluetooth","Alloy Wheels","Navigation System","Remote Start"],
+        "carImage":"https://static.cargurus.com/images/forsale/2021/11/04/05/31/2016_buick_cascada-pic-8502382997933450954-1024x768.jpeg"
+    }]
+}]
+
+
+
+let mileageUL=document.createElement("ul");
+    cars[0].models[0].gasMileage.forEach(function(mileage){
+    let mileageLI=document.createElement("li");
+    mileageLI.innerText=mileage;
+    mileageUL.appendChild(mileageLI);
+    })
+
+    
+document.getElementById('carImg').src = cars[0].models[0].carImage;
+document.getElementById('carName').innerHTML = cars[0].maker;
+document.getElementById('carModel').innerHTML = cars[0].models[0].model;
+document.getElementById('carPrice').innerHTML = cars[0].models[0].price;
+document.getElementById('carFuel').innerHTML = cars[0].models[0].fuelType;
+document.getElementById('carMileage').appendChild(mileageUL);
+
+let mileageeUL=document.createElement("ul");
+cars[1].models[1].gasMileage.forEach(function(mileage){
+let mileageeLI=document.createElement("li");
+mileageeLI.innerText=mileage;
+mileageeUL.appendChild(mileageeLI);
+    
+})
+
+document.getElementById('car_Img').src = cars[1].models[1].carImage;
+document.getElementById('car_Name').innerHTML = cars[1].maker;
+document.getElementById('car_Model').innerHTML = cars[1].models[1].model;
+document.getElementById('car_Price').innerHTML = cars[1].models[1].price;
+document.getElementById('car_Fuel').innerHTML = cars[1].models[1].fuelType;
+document.getElementById('car_Mileage').appendChild(mileageeUL);
+
+
+let carMileageUL=document.createElement("ul");
+cars[3].models[0].gasMileage.forEach(function(mileage){
+let MileageeLI=document.createElement("li");
+MileageeLI.innerText=mileage;
+carMileageUL.appendChild(MileageeLI);
+    
+})
+
+document.getElementById('car-Img').src = cars[3].models[0].carImage;
+document.getElementById('car-Name').innerHTML = cars[3].maker;
+document.getElementById('car-Model').innerHTML = cars[3].models[0].model;
+document.getElementById('car-Price').innerHTML = cars[3].models[0].price;
+document.getElementById('car-Fuel').innerHTML = cars[3].models[0].fuelType;
+document.getElementById('car-Mileage').appendChild(mileageeUL);
